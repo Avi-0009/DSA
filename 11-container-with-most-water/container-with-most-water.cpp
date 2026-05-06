93f509bb56b8eda3952148eb6ae4bdefd56051fa
Avi-0009 <prakharsahu1229@gmail.com>
2026-05-06 12:50:09 +0530

    Time: 0 ms (100.00%) | Memory: 62.8 MB (77.84%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.cpp
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -3,7 +3,7 @@ public:
     int maxArea(vector<int>& height) {
         int maxWater = 0;
         int left = 0, right = height.size() - 1;
-        while(left <= right){
+        while(left < right){
             int length = right - left;
             int breadth = min(height[left], height[right]);
             height[left] > height[right] ? right -- : left ++;
