77185c3b78e1eb7d458b8d6e2d8a07969b2c2409
Avi-0009 <prakharsahu1229@gmail.com>
2026-06-19 12:34:59 +0530

    Time: 4 ms (92.11%) | Memory: 23.8 MB (81.58%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int maxDistinct(string s) {
-        vector<int>uset(26,0);
-        for(char &c : s){
-            uset[c - 'a']++;
-        }
+        vector<int>set(26, 0);
         int count = 0;
-        for(int i : uset){
-            if(i > 0)
+        for(char &c : s){
+            if(set[c - 'a'] == 0){
                 count++;
+                set[c - 'a']++;
+            }
         }
         return count;
     }
