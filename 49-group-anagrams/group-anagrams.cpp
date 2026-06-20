ca3532eb4ba945fee1f80a0c61e9a4e050a44269
Avi-0009 <prakharsahu1229@gmail.com>
2026-06-20 20:16:40 +0530

    Time: 12 ms (83.55%) | Memory: 24.8 MB (87.66%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,21 +1,16 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-
-        unordered_map<string, vector<string>> umap;
-
-        for(string &s : strs){
-            string key = s;
-            sort(key.begin(), key.end());
-            umap[key].push_back(s);
+        unordered_map<string, vector<string>>umap;
+        for(int i = 0; i < strs.size(); i++){
+            string s = strs[i];
+            sort(s.begin(), s.end());
+            umap[s].push_back(strs[i]);
         }
-
-        vector<vector<string>> res;
-
+        vector<vector<string>>res;
         for(auto &pair : umap){
             res.push_back(pair.second);
         }
-        
         return res;
     }
 };
