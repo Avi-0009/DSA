cfa63d0476a59a84b0c630ec700b673afa50c51e
Avi-0009 <prakharsahu1229@gmail.com>
2026-06-21 20:25:30 +0530

    Time: 47 ms (65.15%) | Memory: 29.2 MB (46.39%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -1,39 +1,31 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        vector<vector<int>> res;
+        // if(*min_element(nums.begin(), nums.end()) > 0)return {};
+        // if(*max_element(nums.begin(), nums.end()) < 0)return {};
+
         int n = nums.size();
-
-        if(n < 3) return res;
-
+        vector<vector<int>>res;
         sort(nums.begin(), nums.end());
-
-        for(int i = 0; i < n - 2; i++){
-            if( i > 0 and nums[i] == nums[i - 1])
+        for(int i = 0; i < n - 2; i ++){
+            if(i != 0 and nums[i - 1] == nums[i])
                 continue;
-            
-            // int target = -(nums[i]);
-            
             int left = i + 1, right = n - 1;
             while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                
+                // int mid = left + (right - left) / 2;
+                if(nums[left] + nums[right] + nums[i] > 0){
+                    right--;
 
-                if(sum == 0){
+                }
+                else if(nums[left] + nums[right] + nums[i] < 0){
+                    left ++;
+                }
+                else{
                     res.push_back({nums[i], nums[left], nums[right]});
-                    while(left < right and nums[left] == nums[left + 1])left++;
+                    while(left < right and nums[left] == nums[left + 1])left ++;
                     while(left < right and nums[right] == nums[right - 1])right--;
-                    
                     left++;
                     right--;
-                }
-                
-                else if(sum > 0){
-                    right--;
-                }
-                
-                else{
-                    left ++;
                 }
             }
         }
