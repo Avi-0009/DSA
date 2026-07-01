953d16a51a4b6d59a169ce36dce6b48b44bc6301
Avi-0009 <prakharsahu1229@gmail.com>
2026-07-01 20:17:43 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (58.69%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -6,11 +6,8 @@ public:
         vector<int>left(n, 0), right(n, 0);
         left[0] = nums[0]; left[1] = max(nums[0], nums[1]);
         right[1] = nums[1]; right[2] = max(nums[1], nums[2]);
-        for(int i = 2; i < n - 1; i++){
-            left[i] = max(left[i - 1], nums[i] + left[i - 2]);
-        }
-
         for(int i = 3; i < n; i++){
+            left[i - 1] = max(left[i - 2], nums[i - 1] + left[i - 3]);
             right[i] = max(right[i - 1], nums[i] + right[i - 2]);
         }
 
