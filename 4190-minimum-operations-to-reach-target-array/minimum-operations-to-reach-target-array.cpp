e295f4d0863ae87a421c089d36352cdf69f0b0c0
Avi-0009 <prakharsahu1229@gmail.com>
2026-08-03 14:36:53 +0530

    Time: 84 ms (84.78%) | Memory: 257.3 MB (76.09%) - LeetSync

---
+++ 4190-minimum-operations-to-reach-target-array/minimum-operations-to-reach-target-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, vector<int>& target) {
+        unordered_set<int>set;
+        for(int i =0 ; i < nums.size();i ++){
+            if((nums[i] != target[i]) and (set.find(nums[i]) == set.end())){
+                set.insert(nums[i]);
+            }
+        }
+        return set.size();
+
+    }
+};
